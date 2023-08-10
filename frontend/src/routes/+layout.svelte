cb20df0088072fd5f7202b02cb9a1a321185f45e
Callum <callumcstone@gmail.com>
2023-08-10 18:28:34 +0100

    Basic

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,18 +1,21 @@
 <script>
 	import Header from './Header.svelte';
-	import './styles.css';
 </script>
 
+<!-- SMUI Styles -->
+<link
+  rel="stylesheet"
+  href="node_modules/svelte-material-ui/themes/svelte.css"
+  media="(prefers-color-scheme: light)"
+/>
+<link
+  rel="stylesheet"
+  href="node_modules/svelte-material-ui/themes/svelte-dark.css"
+  media="screen and (prefers-color-scheme: dark)"
+/>
+
 <div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<slot />
 </div>
 
 <style>
@@ -20,34 +23,5 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
